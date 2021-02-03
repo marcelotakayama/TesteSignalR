d784d00bdc179199196d281a26f3d37c5a809c11
marcelotakayama <marcelo.takayama00@gmail.com>
2021-02-03 09:53:53 -0300

    mexendo no arquivo de startup

--- TesteSignalR/Startup.cs
+++ TesteSignalR/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using TesteSignalR.Hubs;
 
 namespace TesteSignalR {
     public class Startup {
@@ -28,6 +29,8 @@ namespace TesteSignalR {
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,6 +46,9 @@ namespace TesteSignalR {
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
+            app.UseSignalR(routes => {
+                routes.MapHub<ChatHub>("/chathub");
+            });
 
             app.UseMvc();
         }
